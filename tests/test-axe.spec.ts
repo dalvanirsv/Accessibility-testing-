921d5669574b6eb4cc6a38f3f06d48da4d079744
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-04-26 23:51:27 -0300

    fix

--- tests/test-axe.spec.ts
+++ tests/test-axe.spec.ts
@@ -1,12 +1,21 @@
-import { test, expect } from '@playwright/test';
-import AxeBuilder from '@axe-core/playwright'; // 1
+import { test as base } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
 
-test.describe('homepage', () => { // 2
-  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('https://your-site.com/'); // 3
+type AxeFixture = {
+  makeAxeBuilder: () => AxeBuilder;
+};
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
+// Extend base test by providing "makeAxeBuilder"
+//
+// This new "test" can be used in multiple test files, and each of them will get
+// a consistently configured AxeBuilder instance.
+export const test = base.extend<AxeFixture>({
+  makeAxeBuilder: async ({ page }, use, testInfo) => {
+    const makeAxeBuilder = () => new AxeBuilder({ page })
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .exclude('#commonly-reused-element-with-known-issue');
 
-    expect(accessibilityScanResults.violations).toEqual([]); // 5
-  });
+    await use(makeAxeBuilder);
+  }
 });
+export { expect } from '@playwright/test';
