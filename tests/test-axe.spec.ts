234aa57e3fb04b971ea35d25c7feb4eb5ec298d1
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-04-27 23:38:33 -0300

    add-new-change

--- tests/test-axe.spec.ts
+++ tests/test-axe.spec.ts
@@ -1,21 +1,12 @@
-import { test as base } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-type AxeFixture = {
-  makeAxeBuilder: () => AxeBuilder;
-};
+test.describe('homepage', () => { 
+  test('to verify aux WCGA rules accessibility issues', async ({ page }) => {
+    await page.goto('https://www.itau.com.br/'); 
 
-// Extend base test by providing "makeAxeBuilder"
-//
-// This new "test" can be used in multiple test files, and each of them will get
-// a consistently configured AxeBuilder instance.
-export const test = base.extend<AxeFixture>({
-  makeAxeBuilder: async ({ page }, use, testInfo) => {
-    const makeAxeBuilder = () => new AxeBuilder({ page })
-      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
-      .exclude('#commonly-reused-element-with-known-issue');
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); // 4
 
-    await use(makeAxeBuilder);
-  }
-});
-export { expect } from '@playwright/test';
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+})
