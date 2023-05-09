826f4bdd625f873d2c78602ac01ed999b3c93310
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-08 23:33:46 -0300

    new-change

--- tests/wcag-test.spec.ts
+++ tests/wcag-test.spec.ts
@@ -4,8 +4,8 @@ test('should not have any automatically detectable WCAG A or AA violations', asy
     await page.goto('https://your-site.com/');
   
     const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+      .withTags(['wcag2a'])
       .analyze();
   
-    expect(accessibilityScanResults.violations);
+    expect(accessibilityScanResults.violations).toEqual(['wcag2a']);
   });
