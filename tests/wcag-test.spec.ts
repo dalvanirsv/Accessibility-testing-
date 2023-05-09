4b87975f9375cd5f59655c1db14df501b70602ac
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-08 23:48:08 -0300

    add-others-wcag

--- tests/wcag-test.spec.ts
+++ tests/wcag-test.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
-    await page.goto('https://your-site.com/');
+    await page.goto('https://fatalmodel.com/');
   
     const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(['wcag2a'])
+      .withTags(['wcag2a','wcag2aa', 'wcag21a', 'wcag21aa'])
       .analyze();
   
-    expect(accessibilityScanResults.violations).toEqual(['wcag2a']);
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
