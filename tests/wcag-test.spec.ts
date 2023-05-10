a17bb86f5346690e32acbbbee498a1b30cf38149
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-09 23:32:32 -0300

    test-creterio

--- tests/wcag-test.spec.ts
+++ tests/wcag-test.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 test('should not have any automatically detectable WCAG A or AA violations', async ({ page }) => {
-    await page.goto('https://fatalmodel.com/');
-  
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(['wcag2a','wcag2aa', 'wcag21a', 'wcag21aa'])
-      .analyze();
-  
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+  await page.goto('https://www.itau.com.br/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
